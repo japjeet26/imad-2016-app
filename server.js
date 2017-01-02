8add9fe37764069316702fcdbefa00587aa06d7f
japjeet26 <japjeet26@gmail.com>
2017-01-02 16:28:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,60 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles = {
+  'article-one':{
+      title: 'article one',
+      heading: 'article one by jap',
+      date: '2nd jan',
+      content: 'content of first article goes here'
+  }, 
+  'article-two':{
+      title: 'article two',
+      heading: 'article two by jap',
+      date: '2nd jan',
+      content: 'content of second article goes here' 
+  },
+  'article-three':{
+      title: 'article three',
+      heading: 'article three by jap',
+      date: '2nd jan',
+      content: 'content of third article goes here'   
+      
+  }
+  
+}
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=`
+    <html>
+        <head>
+             <title>
+               ${title}
+             </title>
+             <meta name = "viewport"  content="width-device-width , initial-scale=1" />
+         </head>
+         <body>
+             ${date}
+             <h3>
+             ${heading}
+             </h3>
+             <p>
+             ${content}
+             </p>
+         </body>
+     </html>
+    `;
+return htmlTemplate;
+    
+    
+}
+
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -13,20 +67,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','article_two.html'));
-});
-app.get('/article-three', function (req, res) {
-  res.send("article 3 is served here");
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
