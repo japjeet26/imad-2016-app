4b89558cac96f66cb5ca99585623ec1cc7147ef9
japjeet26 <japjeet26@gmail.com>
2017-01-03 19:16:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 alert("hello@");
 var maadi= document.getElementById('maadi');
 maadi.onclick = function(){
-    maadi.style.margin = "100px";
+    maadi.style.margin-left = "100px";
 };
